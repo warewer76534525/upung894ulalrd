7ad2cc59d996e0e9c32348a7c082a49272d4e8e5
welly87 <if05041@gmail.com>
2011-03-03 13:28:46 +0700

    delete file chk from ftp

--- src/com.ocbcmcd.confirmwatcher/test/com/ocbcmcd/confirmwatcher/ftpchecker/When_get_all_file_in_directory.java
+++ src/com.ocbcmcd.confirmwatcher/test/com/ocbcmcd/confirmwatcher/ftpchecker/When_get_all_file_in_directory.java
@@ -36,4 +36,30 @@ public class When_get_all_file_in_directory {
 		FTPFileChecker fileChecker = context.getBean(FTPFileChecker.class);
 		Assert.assertFalse(fileChecker.fileExist("movie-hunt2.txt"));
 	}
+	
+	@Test
+	public void Should_delete_file_in_server() throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("ftp-retriever.xml");
+		FTPFileChecker fileChecker = context.getBean(FTPFileChecker.class);
+		Assert.assertTrue(fileChecker.fileExist("movie-hunt.txt.chk"));
+		
+		fileChecker.deleteFile("movie-hunt.txt.chk");
+		
+		Assert.assertFalse(fileChecker.fileExist("movie-hunt.txt.chk"));
+	}
+	
+	@Test
+	public void Should_check_session_different() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("ftp-retriever.xml");
+		DefaultFtpSessionFactory sessionFactory = context.getBean(DefaultFtpSessionFactory.class);
+		Session session = sessionFactory.getSession();
+		Session session2 = sessionFactory.getSession();
+		
+		Assert.assertNotSame(session, session2);
+		
+		session.close();
+		session2.close();
+	}
+	
+	
 }
