764f859d51e2769739a19b0865c922c6d0214655
welly87 <if05041@gmail.com>
2011-03-08 17:15:23 +0700

    adding mail sending for failed process file

---
+++ src/com.ocbcmcd.mailsender/test/com/ocbcmcd/mailsender/mail/When_send_email_to_mcdonaldclient.java
@@ -0,0 +1,20 @@
+package com.ocbcmcd.mailsender.mail;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailSender;
+
+public class When_send_email_to_mcdonaldclient {
+	
+	@Test
+	public void Should_send_email_with_error_message() {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+			"mail-sender-test.xml");
+		MailSender mailSender = (MailSender)applicationContext.getBean("mailSender");	
+		Assert.assertNotNull(mailSender);
+		
+		MailService mailService = applicationContext.getBean(MailService.class);
+		mailService.sendErrorMessage("error file yg mengerikan");
+	}
+}
