962822c248779b1569756d8878513d394636f0fa
adisembiring <sembiring.adi@gmail.com>
2011-03-16 06:24:56 -0700

    add HouseKeepingService and Handler

--- src/com.ocbcmcd.housekeeping/src/com/ocbcmcd/housekeeping/service/IHouseKeepingService.java
+++ src/com.ocbcmcd.housekeeping/src/com/ocbcmcd/housekeeping/service/IHouseKeepingService.java
@@ -1,12 +1,17 @@
 package com.ocbcmcd.housekeeping.service;
 
 import com.ocbcmcd.message.EncryptedFileSending;
+import com.ocbcmcd.message.OcbcFileProcessFailed;
 import com.ocbcmcd.message.OcbcFileProcessedSucessfully;
+import com.ocbcmcd.message.OcbcFileSendingFailed;
+import com.ocbcmcd.message.SapFileDuplicated;
 
 public interface IHouseKeepingService {
 
 	void moveSuccessFile(OcbcFileProcessedSucessfully fileProcessedEvent);
-
 	void moveOnProcessingFile(EncryptedFileSending event);
-
+	void moveOnDuplicatedFile(SapFileDuplicated event);
+	void moveOnSendingFailed(OcbcFileSendingFailed event);
+	void moveOnProcessedFailed(OcbcFileProcessFailed event);;
+	
 }
