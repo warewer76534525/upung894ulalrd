f824e75bcc557d094d578563d6dccb3b2d693ffb
adisembiring <sembiring.adi@gmail.com>
2011-03-16 06:25:27 -0700

    add HouseKeepingService and Handler

---
+++ src/com.ocbcmcd.housekeeping/src/com/ocbcmcd/housekeeping/service/impl/MoveOnDuplicatedFileHandler.java
@@ -0,0 +1,40 @@
+package com.ocbcmcd.housekeeping.service.impl;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.support.JmsUtils;
+import org.springframework.stereotype.Service;
+
+import com.ocbcmcd.housekeeping.service.IHouseKeepingService;
+import com.ocbcmcd.message.SapFileDuplicated;
+
+@Service
+public class MoveOnDuplicatedFileHandler implements MessageListener {
+	protected Log log = LogFactory.getLog(getClass());
+	
+	@Autowired
+	IHouseKeepingService houseKeepingService;
+
+	@Override
+	public void onMessage(Message message) {
+		ObjectMessage mapMessage = (ObjectMessage) message;
+		SapFileDuplicated event;
+		try {
+			event = (SapFileDuplicated) mapMessage.getObject();
+			log.info("incoming event: " + event);
+			Thread.sleep(1000 * 5);
+			houseKeepingService.moveOnDuplicatedFile(event);
+		} catch (JMSException e) {
+			throw JmsUtils.convertJmsAccessException(e);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+}
---
+++ src/com.ocbcmcd.housekeeping/test/com/ocbcmcd/housekeeping/When_sending_failed_recieved.java
@@ -0,0 +1,69 @@
+package com.ocbcmcd.housekeeping;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ocbcmcd.housekeeping.service.IHouseKeepingService;
+import com.ocbcmcd.message.OcbcFileSendingFailed;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/application-context.xml")
+public class When_sending_failed_recieved {
+	private static final String FILE_NAME = "sendingFailed.txt";
+	
+	private OcbcFileSendingFailed event;
+	
+	@Autowired
+	private IHouseKeepingService houseKeepingService;
+	
+	@Value("${processing.dir}")
+	private String processingDirectory;
+	
+	@Value("${encrypted.dir}")
+	private String encryptedDirectory;
+	
+	@Value("${encrypted.ext}")
+	private String encryptedExt;
+	
+	@Value("${failed.dir}")
+	private String failedDirectory;
+	
+	@Before
+	public void setUp() {
+		Logger.getRootLogger().setLevel(Level.INFO);
+		Logger.getLogger("org.springframework").setLevel(Level.WARN);
+		BasicConfigurator.configure();
+				
+		event = new OcbcFileSendingFailed(FILE_NAME, "Error Message", "Stacktrace");
+	}
+
+	
+	@Test
+	public void should_move_file_from_incoming_to_processing() throws IOException {
+		File processing = new File(processingDirectory, FILE_NAME);
+		File failed = new File(failedDirectory, FILE_NAME);
+		File encrypted = new File(encryptedDirectory, FILE_NAME + encryptedExt);
+		
+		houseKeepingService.moveOnSendingFailed(event);
+		
+		System.out.println(processing.getAbsolutePath());
+		System.out.println(failed.getAbsolutePath());
+		
+		Assert.assertFalse(processing.exists());
+		Assert.assertTrue(failed.exists());
+		Assert.assertTrue(!encrypted.exists());
+	}
+}
