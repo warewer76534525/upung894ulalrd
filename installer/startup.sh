cfd7d65e1d788cc89f7eb61c27e355dccfc796f0
adisembiring <sembiring.adi@gmail.com>
2011-04-11 14:02:57 -0700

    configure build for all platform

--- installer/startup.sh
+++ installer/startup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-java -jar bin/confirmwatcher-service/confirmwatcher-service.jar &
-java -jar bin/ftpfilesender-service/ftpfilesender-service.jar &
-java -jar bin/housekeeping-service/housekeeping-service.jar &
-java -jar bin/mailsender-service/mailsender-service.jar &
-java -jar bin/sapfilewatcher-service/sapfilewatcher-service.jar &
+ant startup
